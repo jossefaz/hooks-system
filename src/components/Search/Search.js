b5d4c81f2167df6618a6e3ada375b97e21f71216
jossefaz <jossefaz@protonmail.com>
2020-12-29 14:53:00 +0200

    update search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,8 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 500);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -13,19 +23,25 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-    if (term) {
-      search();
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const rendererdResults = results.map((res) => {
     return (
       <div key={res.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${res.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{res.title}</div>
           <span dangerouslySetInnerHTML={{ __html: res.snippet }}></span>
