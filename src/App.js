5e42df0ec63e9cdb5b4be3d2c5f27070638ab1d4
jossefaz <jossefaz@protonmail.com>
2020-12-29 15:13:15 +0200

    init dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Accordion from "./components/Accordion/Accordion";
 import Search from "./components/Search/Search";
-
+import DropDown from "./components/DropDown/DropDown";
+import { useState } from "react";
 const items = [
   {
     title: "What is react",
@@ -15,10 +16,31 @@ const items = [
     content: "With component",
   },
 ];
+
+const options = [
+  {
+    label: "The color red",
+    value: "red",
+  },
+  {
+    label: "The color Green",
+    value: "green",
+  },
+  {
+    label: "A shade of blue",
+    value: "blue",
+  },
+];
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <DropDown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
